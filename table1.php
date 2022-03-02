afc6c17d7953a7c5f9826a6583bfd9fdec601bc3
THitipornn56647 <97584461+THitipornn56647@users.noreply.github.com>
2022-03-02 15:52:19 +0700

    Create table1.php

---
+++ table1.php
@@ -0,0 +1,45 @@
+<html>
+  <head>
+    <title>การสร้างตาราง</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>Twitter/FB</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>ซง จุง-กี (Song Joong-ki)</td>
+        <td><img src="https://www.brighttv.co.th/wp-content/uploads/2022/01/Song-Joong-Ki-241121-822x1024-1.jpg" width="45px"></td>
+        <td><a href="https://th-th.facebook.com/JoongKi.intl">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>คิม จี-ซู (김지수)</td>
+        <td><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/210928_Jisoo.jpg/250px-210928_Jisoo.jpg" width="45px"></td>
+        <td><a href="https://th-th.facebook.com/JisooBPThailand/">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>อี จง-ซ็อก (Lee Jong-suk)</td>
+        <td><img src="https://i.pinimg.com/originals/c8/72/b6/c872b69a90c80f6b869b5ce8c92458f3.jpg" width="45px"></td>
+        <td><a href="https://th-th.facebook.com/LeeJongSukWorld/">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>พัก มิน-ย็อง (Park Min-young)</td>
+        <td><img src="https://teen.mthai.com/app/uploads/2018/07/Park-Min-young-10.jpg" width="45px"></td>
+        <td><a href="https://web.facebook.com/people/Park-Min-Young-%E0%B8%9B%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%84%E0%B8%A1%E0%B8%B4%E0%B8%99%E0%B8%A2%E0%B8%AD%E0%B8%87-Fans/100044695971220/">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>ซงคัง (Song Kang)</td>
+        <td><img src="https://www.brighttv.co.th/wp-content/uploads/2021/12/1640264574-20211223-songkang.jpg" width="45px"></td>
+        <td><a href="https://web.facebook.com/SupportSongKang/?_rdc=1&_rdr">Facebook</a></td>
+      </tr>
+    </table>
+  </body>
+</html>
